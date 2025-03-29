11525f3d82981718393bf109e3638cf3466ab36c
JoseV <josjosvarmam912@gmail.com>
2025-03-28 22:03:47 -0400

    AntesTarea

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v2')
+  app.useGlobalPipes(
+    new ValidationPipe({
+    whitelist: true, // Elimina propiedades no definidas en el DTO
+    forbidNonWhitelisted: true, // Lanza error si hay propiedades no permitidas
+    transform: true, // Convierte payloads en instancias de DTOs
+    })
+  );
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
+
